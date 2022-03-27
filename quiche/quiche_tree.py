daff2d467ffab45a32a4185561eb9985ac46ebbe
riswords <riswords@gmail.com>
2022-03-27 03:14:59 -0500

    Add method to generate SVG from QuicheTree
    Based on zdevito's e-graphs Colab

--- quiche/quiche_tree.py
+++ quiche/quiche_tree.py
@@ -26,3 +26,40 @@ class QuicheTree(ABC):
             )
         else:
             return str(self.value())
+
+    def _repr_svg_(self):
+        from graphviz import Digraph
+
+        def escape(x):
+            escapes = [('|', '\\|'),
+                       ('<', '\\<'),
+                       ('>', '\\>')]
+            x = str(x)
+            for old, new in escapes:
+                x = x.replace(old, new)
+            # return str(x).replace('|', '\\|').replace('<', '\\<').replace('>', '\\>')
+            return x
+
+        def make_node(graph, x):
+            node_id = str(id(x))
+            graph.node(f'{node_id}', label=f'{escape(x.value())}', shape='rectangle')
+            node_children = x.children()
+            if node_children:
+                for child in node_children:
+                    child_id = make_node(graph, child)
+                    graph.edge(f'{node_id}', f'{child_id}')
+            return node_id
+
+        graph = Digraph(node_attr={'shape': 'record', 'height': '.1'})
+        make_node(graph, self)
+        return graph._repr_image_svg_xml()
+
+    def write_to_svg(self, filename):
+        """
+        Write QuicheTree to file in SVG format.
+
+        :param filename: filename to write
+        :return: None
+        """
+        with open(filename, "w") as f:
+            f.write(self._repr_svg_())
